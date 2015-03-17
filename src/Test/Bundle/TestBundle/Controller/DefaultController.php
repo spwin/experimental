65b4f33ce6315610de6d379027679e5508465e3a
spwin <info@pixsens.lt>
2015-03-17 06:21:44 +0200

    Chat beginning

--- src/Test/Bundle/TestBundle/Controller/DefaultController.php
+++ src/Test/Bundle/TestBundle/Controller/DefaultController.php
@@ -6,14 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;	
 use Test\Bundle\TestBundle\Entity\Post;
+use Test\Bundle\TestBundle\Entity\Messages;
 
 class DefaultController extends Controller
 {
     /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $limit = 30;
+        $messages = $em->getRepository('TestTestBundle:Messages')->findBy(
+            array(),
+            array('date' => 'desc'),
+            $limit
+        );
+
+        $username = 'Testuser';
+        $message = new Messages();
+        $form = $this->createFormBuilder($message)
+            ->add('user', 'hidden',array('data' => $username))
+            ->add('message', 'text')
+            ->add('save', 'submit', array('label' => 'Send'))
+            ->getForm();
+        return array('messages' => $messages, 'form' => $form->createView());
+    }
+
+    /**
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
+    public function helloAction($name)
     {
 		$em = $this->getDoctrine()->getManager();
 
---
+++ src/Test/Bundle/TestBundle/Entity/Messages.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Test\Bundle\TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Messages
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Test\Bundle\TestBundle\Entity\MessagesRepository")
+ */
+class Messages
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user", type="string", length=128)
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="date", type="integer")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     * @return Messages
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Messages
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set date
+     *
+     * @param integer $date
+     * @return Messages
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return integer 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
